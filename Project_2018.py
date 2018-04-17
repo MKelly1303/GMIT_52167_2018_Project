e1f5f5e632d475505ed1d7a55003f8d340419800
Mark Kelly <G00364705@gmit.ie>
2018-04-17 21:45:19 +0100

    Added pivot tables for each class

--- Project_2018.py
+++ Project_2018.py
@@ -1,11 +1,12 @@
 # Mark Kelly  - 2018/04/09
 
+ 
+
 # Importing Test with iris data
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 import pandas as pd
-
 
 data = pd.read_csv("data/iris.csv", names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
 
@@ -14,26 +15,41 @@ sw = data['sepal_width']
 pl = data['petal_length']
 pw = data['petal_width']
 cl = data['class']
+ 
 
 #print(data)
 print('Mean Sepal Length = ', '{:01.2f}'.format(np.mean(sl)))
 print('Max Sepal Length = ', '{:01.2f}'.format(np.max(sl)))
 print('Min Sepal Length = ', '{:01.2f}'.format(np.min(sl)))
 
-plt.figure(1)
-plt.title('Sepal Length Histogram')
-plt.xlabel('mm')
-#plt.subplot(211)
-plt.hist(sl)
+ 
 
-#plt.subplot(212)
-#plt.hist(sw)
-
-#plt.subplot(213)
-#plt.hist(pl)
-
-#plt.subplot(214)
-#plt.hist(pw)
-
+#f, ((ax1, ax2, ax3, ax4)) = plt.subplots(1, 4, sharex='col', sharey='row')
+#ax1.hist(sl)
+#ax1.set_title('Sepal Length')
+#ax2.hist(s)
+#ax2.set_title('Sepal Width')
+#ax3.hist(pl)
+#ax3.set_title('Petal Length')
+#ax4.hist(pw)
+#ax4.set_title('Petal Width')
+ 
+plt.scatter(sw,sl, color='red')
+plt.xlabel('Sepal Width')
+plt.ylabel('Sepal Length')
 plt.show()
 
+data['class']=data['class'].astype("category")
+data["class"].cat.set_categories(["Iris-setosa","Iris-versicolor","Iris-virginica"],inplace=True)
+
+df = pd.pivot_table(data,index=['class'], aggfunc='mean')
+print("Mean Values for each class...")
+print(df)
+
+df2 = pd.pivot_table(data,index=['class'], aggfunc='min')
+print("Min Values for each class...")
+print(df2)
+
+df3 = pd.pivot_table(data,index=['class'], aggfunc='max')
+print("Max Values for each class...")
+print(df3)
