de2210ddcc91b91eedad405e9631a44abe7cd6fc
MKelly1303 <35726423+MKelly1303@users.noreply.github.com>
2018-04-24 22:36:04 +0100

    Added comments, generated mean, min and max values
    Added comments, generated mean, min and max values for each column. rearranged the chart so it is generated at the end.

--- Project_2018.py
+++ Project_2018.py
@@ -2,29 +2,52 @@
 
 
 import matplotlib.pyplot as plt
-import matplotlib as mpl
+#import matplotlib as mpl
 import numpy as np
 import pandas as pd
 
 data = pd.read_csv("data/iris.csv", names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
-	
+
+#Importing the data set into an array called data and naming each column.
+
 sl = data['sepal_length']
 sw = data['sepal_width']
 pl = data['petal_length']
 pw = data['petal_width']
 cl = data['class']
+
+print('       Sepal Length   Sepal Width      Petal Length     Petal Width')
+
+print('Mean  = ', '{:01.2f}'.format(np.mean(sl)),'          ', '{:01.2f}'.format(np.mean(sw)), '            ', '{:01.2f}'.format(np.mean(pl)),'            ', '{:01.2f}'.format(np.mean(pw)))
+print('Min   = ', '{:01.2f}'.format(np.min(sl)),'          ', '{:01.2f}'.format(np.min(sw)), '            ', '{:01.2f}'.format(np.min(pl)),'            ', '{:01.2f}'.format(np.min(pw)))
+print('Max   = ', '{:01.2f}'.format(np.max(sl)),'          ', '{:01.2f}'.format(np.max(sw)), '            ', '{:01.2f}'.format(np.max(pl)),'            ', '{:01.2f}'.format(np.max(pw)))
 	
+
 	
-#print(data)
-print('Mean Sepal Length = ', '{:01.2f}'.format(np.mean(sl)))
-print('Max Sepal Length = ', '{:01.2f}'.format(np.max(sl)))
-print('Min Sepal Length = ', '{:01.2f}'.format(np.min(sl)))
+data['class']=data['class'].astype("category")
+data["class"].cat.set_categories(["Iris-setosa","Iris-versicolor","Iris-virginica"],inplace=True)
 	
+df = pd.pivot_table(data,index=['class'], aggfunc='mean')
+print("Mean Values for each class...")
+print(df)
+	
+df2 = pd.pivot_table(data,index=['class'], aggfunc='min')
+print("Min Values for each class...")
+print(df2)
+	
+df3 = pd.pivot_table(data,index=['class'], aggfunc='max')
+print("Max Values for each class...")
+print(df3)
+
 groups=data[['class','sepal_length','sepal_width', 'petal_length', 'petal_width']].groupby('class')
-	
+
+#Creating a group that is differeniated on the different values of 'class'
+
 fig, ((ax1, ax2, ax3, ax4), (ax5, ax6, ax7, ax8),(ax9, ax10, ax11, ax12),(ax13, ax14, ax15, ax16)) = plt.subplots(4, 4, sharex='col', sharey='row')
+# Creating a 4 x 4 subplot matix with plots names ax1 to ax16 where each row and column will share the axis'.
  
-#https://i.stack.imgur.com/R0qI4.jpg TRYING TO recreate this image.
+#https://stackoverflow.com/questions/42592493/displaying-pair-plot-in-pandas-data-frame/45195783 Trying to recreate the images seen on this page.
+
 for name, group in groups: 
     ax1.hist(group.sepal_length,label=name)
     ax2.scatter(group.sepal_width,group.sepal_length,label=name)
@@ -57,24 +80,5 @@ ax16.set_xlabel('Petal Width(cm)')
 ax6.legend()
 ax11.legend()
 ax16.legend()
-#ax5.legend()
-#ax6.legend()
-#ax7.legend()
-#ax8.legend()
 
 plt.show()
-	
-data['class']=data['class'].astype("category")
-data["class"].cat.set_categories(["Iris-setosa","Iris-versicolor","Iris-virginica"],inplace=True)
-	
-df = pd.pivot_table(data,index=['class'], aggfunc='mean')
-print("Mean Values for each class...")
-print(df)
-	
-df2 = pd.pivot_table(data,index=['class'], aggfunc='min')
-print("Min Values for each class...")
-print(df2)
-	
-df3 = pd.pivot_table(data,index=['class'], aggfunc='max')
-print("Max Values for each class...")
-print(df3)
