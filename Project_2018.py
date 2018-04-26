1ad5ebdf7c8d1c0bc61171dead2d15dc7bd2a443
MKelly1303 <35726423+MKelly1303@users.noreply.github.com>
2018-04-26 21:28:15 +0100

    Update Project_2018.py
    Simplified the pivot tables. Added sum calculations, added more comments, removed from libraries that weren't used and put my name to it.

--- Project_2018.py
+++ Project_2018.py
@@ -1,13 +1,12 @@
-# Importing Test with iris data
+# Mark Kelly - 26/04/2018 - GMIT - 52167 - Final Project
 
 
 import matplotlib.pyplot as plt
-#import matplotlib as mpl
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("data/iris.csv", names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
 
+data = pd.read_csv("data/iris.csv", names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
 #Importing the data set into an array called data and naming each column.
 
 sl = data['sepal_length']
@@ -15,37 +14,46 @@ sw = data['sepal_width']
 pl = data['petal_length']
 pw = data['petal_width']
 cl = data['class']
+# creating a shortnamed variable for each data column.
 
+# Print headings for each data type.
 print('        Sepal Length   Sepal Width      Petal Length       Petal Width')
+# Print headings for each data type.
 
 print('Mean  =   ', '{:01.2f}'.format(np.mean(sl)),'          ', '{:01.2f}'.format(np.mean(sw)), '            ', '{:01.2f}'.format(np.mean(pl)),'            ', '{:01.2f}'.format(np.mean(pw)))
-print('Min   =   ', '{:01.2f}'.format(np.min(sl)),'          ', '{:01.2f}'.format(np.min(sw)), '            ', '{:01.2f}'.format(np.min(pl)),'            ', '{:01.2f}'.format(np.min(pw)))
-print('Max   =   ', '{:01.2f}'.format(np.max(sl)),'          ', '{:01.2f}'.format(np.max(sw)), '            ', '{:01.2f}'.format(np.max(pl)),'            ', '{:01.2f}'.format(np.max(pw)))
+# generating and printing the mean value for each data column and formatting it to 2 decimal places.
 
+print('Min   =   ', '{:01.2f}'.format(np.min(sl)),'          ', '{:01.2f}'.format(np.min(sw)), '            ', '{:01.2f}'.format(np.min(pl)),'            ', '{:01.2f}'.format(np.min(pw)))
+# generating and printing the min value for each data column and formatting it to 2 decimal places.
+
+print('Max   =   ', '{:01.2f}'.format(np.max(sl)),'          ', '{:01.2f}'.format(np.max(sw)), '            ', '{:01.2f}'.format(np.max(pl)),'            ', '{:01.2f}'.format(np.max(pw)))
+# generating and printing the max value for each data column and formatting it to 2 decimal places.
 	
-data['class']=data['class'].astype("category")
-data["class"].cat.set_categories(["Iris-setosa","Iris-versicolor","Iris-virginica"],inplace=True)
-	
-df = pd.pivot_table(data,index=['class'], aggfunc='mean')
+print('Sum   =  ', '{:01.1f}'.format(np.sum(sl)),'         ', '{:01.1f}'.format(np.sum(sw)), '           ', '{:01.1f}'.format(np.sum(pl)),'           ', '{:01.1f}'.format(np.sum(pw)))
+# generating and printing the sum value for each data column and formatting it to 1 decimal places.
+
+
 print("Mean Values for each class...")
-print(df)
-	
-df2 = pd.pivot_table(data,index=['class'], aggfunc='min')
+print(pd.pivot_table(data, index=['class'], aggfunc='mean'))
+print(" ")
 print("Min Values for each class...")
-print(df2)
-	
-df3 = pd.pivot_table(data,index=['class'], aggfunc='max')
+print(pd.pivot_table(data, index=['class'], aggfunc='min'))
+print(" ")
 print("Max Values for each class...")
-print(df3)
+print(pd.pivot_table(data, index=['class'], aggfunc='max'))
+print(" ")
+print("Sum Values for each class...")
+print(pd.pivot_table(data, index=['class'], aggfunc='sum'))
+	
 
 groups=data[['class','sepal_length','sepal_width', 'petal_length', 'petal_width']].groupby('class')
-
-#Creating a group that is differeniated on the different values of 'class'
+#Creating a group that is differeniated and grouped on the different values of 'class'
 
 fig, ((ax1, ax2, ax3, ax4), (ax5, ax6, ax7, ax8),(ax9, ax10, ax11, ax12),(ax13, ax14, ax15, ax16)) = plt.subplots(4, 4, sharex='none', sharey='none')
-# Creating a 4 x 4 subplot matix with plots names ax1 to ax16 where each row and column will share the axis'.
+# Creating a 4 x 4 subplot matix with plots names ax1 to ax16 where each row and column will not share the axis'.      
  
-#https://stackoverflow.com/questions/42592493/displaying-pair-plot-in-pandas-data-frame/45195783 Trying to recreate the images seen on this page.
+#https://stackoverflow.com/questions/42592493/displaying-pair-plot-in-pandas-data-frame/45195783 Trying to recreate the images seen on this page as I felt it shows a good visual representation of the differences between the classes.
+
 
 for name, group in groups: 
     ax1.hist(group.sepal_length,label=name)
@@ -67,6 +75,8 @@ for name, group in groups:
     ax14.scatter(group.sepal_width,group.petal_width,label=name)
     ax15.scatter(group.petal_length,group.petal_width,label=name)
     ax16.hist(group.petal_width,label=name)
+# Using a for loop to generate each plot where they will be grouped by the value in class. For each data column, there will be be a histogram generated along with a scatterplot against all other data type. They will be coloured differently depending on the value of class. As there are 3 flower types in our class column, there will be 3 colours each depicting the different flower types.
+
 ax1.legend()
 ax1.set_ylabel('Sepal Length(cm)')
 ax5.set_ylabel('Sepal Width(cm)')
@@ -79,5 +89,7 @@ ax16.set_xlabel('Petal Width(cm)')
 ax6.legend()
 ax11.legend()
 ax16.legend()
+# Setting the legend to appear on each histogram. Labels will only be generated once for each row and column on the left of the charts and at the bottom.
 
 plt.show()
+# Plot the chart
